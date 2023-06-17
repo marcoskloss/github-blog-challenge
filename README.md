9ec9e6cb2a4313a9a49a064b26f3bd87309f8998
Marcos Kloss <marcoskloss2001@gmail.com>
2023-06-16 23:50:34 -0300

    add project initial structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rocketseat challenge - Github Blog
+
+https://efficient-sloth-d85.notion.site/Desafio-03-Github-Blog-13593953670346908462ddc648d42cf1
---
+++ src/Routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+])
+
+function Routes() {
+  return (
+    <RouterProvider router={router} />
+    )
+}
+
+export default Routes
+
